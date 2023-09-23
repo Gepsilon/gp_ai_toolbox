292f5c6f8047c529c8cf955e763a3c6ba383a535
kadimouaad <kadi.mouaad@gmail.com>
2023-09-24 00:59:20 +0100

    moved models to new directory
    i moved all the models to new directory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from ultralytics import YOLO
 import os
+from pathlib import Path
+
+
 #  import super_gradients
 #  model = models.get("model-name", pretrained_weights="pretrained-model-name")
 #  error when trying to install YOLONAS : Failed building wheel for pycocotools
@@ -7,24 +9,23 @@ import os
 
 def inference(chosen_model, data_set):
     if chosen_model == "YOLOv8":
+        from ultralytics import YOLO
         model = YOLO('yolov8n.pt')
         results = model(data_set, save=True)
         return results
     if chosen_model == "YOLOv5":
-        os.system(f'python yolov5\detect.py --source={data_set}')
-        #  need a save in custom file
+        from Models_zoo.yolov5.detect import run
+        run(source=data_set)
 
     if chosen_model == "YOLOv6":
-        os.chdir('C:\object_detection_lib\YOLOv6')
-        os.system(f'python tools/infer.py --weights yolov6s.pt --source {data_set}')
-        os.chdir('C:\object_detection_lib')
-        #  need a save in custom file
+        from Models_zoo.YOLOv6.tools.infer import run
+        os.chdir(f'{Path(__file__).parent}/YOLOv6')
+        print(os.getcwd())
+        run(source=data_set)
 
     if chosen_model == "YOLOv7":
-        os.chdir('C:\object_detection_lib\yolov7')
-        os.system(f'python detect.py --weights yolov7.pt --source {data_set}')
-        os.chdir('C:\object_detection_lib')
-        #  need a save in custom file
+        from Models_zoo.yolov7.detect import detect
+        detect()
 
     if chosen_model == "YOLOvR":
         os.chdir('C:\object_detection_lib\yolor')
@@ -33,7 +34,7 @@ def inference(chosen_model, data_set):
         #  need a save in custom file
         #  work only in cuda env
 
-
+inference('YOLOv7','m')
 
 
 
