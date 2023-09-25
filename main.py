3029dbfa53a4af3238acdab94bc8ed2e5093dbc3
kadimouaad <kadi.mouaad@gmail.com>
2023-09-25 22:55:10 +0100

    model zoo and import problem
    model zoo and import problem
    i also changed data sample name with underscore

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
-from ultralytics import YOLO
-import os
-#  import super_gradients
-#  model = models.get("model-name", pretrained_weights="pretrained-model-name")
-#  error when trying to install YOLONAS : Failed building wheel for pycocotools
+import sys, os
+
+
+def is_python_module(path):
+    return os.path.isfile(os.path.join(path, '__init__.py'))
+
+
+def load_python_modules(path):
+    [sys.path.append(os.path.abspath(x[0])) for x in os.walk(path) if is_python_module(x[0])]
 
 
 def inference(chosen_model, data_set):
     if chosen_model == "YOLOv8":
+        from ultralytics import YOLO
         model = YOLO('yolov8n.pt')
         results = model(data_set, save=True)
         return results
     if chosen_model == "YOLOv5":
-        os.system(f'python yolov5\detect.py --source={data_set}')
-        #  need a save in custom file
+        from yolov5.detect import run
+        run()
 
     if chosen_model == "YOLOv6":
-        os.chdir('C:\object_detection_lib\YOLOv6')
-        os.system(f'python tools/infer.py --weights yolov6s.pt --source {data_set}')
-        os.chdir('C:\object_detection_lib')
-        #  need a save in custom file
+        yolo_v6_path = 'YOLOv6'
+        sys.path.append(os.path.abspath(yolo_v6_path))
+        load_python_modules(yolo_v6_path)
+        from YOLOv6.tools import infer
+        os.chdir(yolo_v6_path)
+        infer.run(weights='yolov6s.pt', yaml='D:\object_detection_lib\YOLOv6\data\dataset.yaml', source=data_set,
+                  hide_labels=True)
 
     if chosen_model == "YOLOv7":
-        os.chdir('C:\object_detection_lib\yolov7')
-        os.system(f'python detect.py --weights yolov7.pt --source {data_set}')
-        os.chdir('C:\object_detection_lib')
-        #  need a save in custom file
-
-    if chosen_model == "YOLOvR":
-        os.chdir('C:\object_detection_lib\yolor')
-        os.system(f'python detect.py --source {data_set} --cfg cfg/yolor_p6.cfg --weights yolor_p6.pt')
-        os.chdir('C:\object_detection_lib')
-        #  need a save in custom file
-        #  work only in cuda env
+        yolo_v7_path = 'YOLOv7'
+        sys.path.append(os.path.abspath(yolo_v7_path))
+        load_python_modules(yolo_v7_path)
+        os.chdir(yolo_v7_path)
+        os.system(f'python detect.py --weights yolov7.pt ')
 
 
-
-
+if __name__ == '__main__':
+    inference()
 
